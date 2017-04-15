4282aa4d60943e31cf64ca03a81f8e7198b96d65
lc-soz <lclpsoz@gmail.com>
2017-04-14 23:01:55 -0300

    Run through every possible permutation of sequences with unitary values (using index). limited to three values and three spaces

--- Source/run.py
+++ Source/run.py
@@ -1,9 +1,24 @@
 import math
+import time
 
-seqMax = 13
-values = [16, 18, 19, 20, 21, 24, 25, 30]
+seqMax = 3
+values = [1, 3, 7]
 valuesQnt = len(values)
-targetValue = 251
+targetValue = 10
+
+def seqTotalSize(val):
+	seqTotalSize = 0
+	for i in val:
+		if i != None:
+			seqTotalSize += seqSingle[i][0][1]
+	return seqTotalSize 
+
+def seqTotalValue(val):
+	seqTotalValue = 0
+	for i in val:
+		if i != None:
+			seqTotalValue += seqSingle[i][0][0] * seqSingle[i][0][1]
+	return seqTotalValue
 
 # Amount of possible combinations with repetition per sequence size
 
@@ -29,11 +44,48 @@ seqSingleQnt = seqMax * valuesQnt
 seqSingle = []
 c = 0
 while c < seqSingleQnt:
-	seqSingleSize = (c % 13) + 1
-	valueIndex = c / 13
-	seqSingle.append([[values[valueIndex], seqSingleSize], values[valueIndex] * seqSingleSize])
-	print(str(c) + "|" + str(seqSingleSize) + "|" + str(valueIndex))
+	seqSingleSize = (c % seqMax) + 1
+	valuesIndex = c / seqMax
+	seqSingle.append([[values[valuesIndex], seqSingleSize], values[valuesIndex] * seqSingleSize])
+	print(str(c) + "|" + str(valuesIndex) + "|" + str(seqSingleSize))
 	print(str(seqSingle) + '\n')
 	c += 1
+
+# Combination of single sequences to match target value
+
+seqTest = [None] * seqMax
+seqFinal = []
+a = 0
+ver = True
+stg = -1
+while ver == True:
+	
+	seqTest[-1] = -1
+	testOverFl = False
+	while (not(all(isinstance(item, int) for item in seqTest)) or seqTest[0] < 8 or sum(seqTest) < (len(seqSingle) - 1) * seqMax):
+
+		
+		if seqTest[stg] < 9:
+			seqTest[stg] += 1
+		
+		if seqTest[stg] == 9:
+			testOverFl = True
+		
+		while testOverFl:
+			seqTest[stg] = 0
+			if seqTest[stg - 1] == None:
+				seqTest[stg - 1] = 0
+			else:
+				seqTest[stg - 1] += 1
+			if seqTest[stg - 1] == 9:
+				stg -= 1
+			else:
+				stg = -1
+				testOverFl = False		
+		a += 1
+		print(str(a) + ': AFT: ' + str(seqTest) + " | " + str(stg))
+
+	if (not(all(isinstance(item, int) for item in seqTest)) or (sum(seqTest) == (len(seqSingle) - 1) * seqMax)):
+		ver = False
 
 
