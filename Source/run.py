e216e873323ae62b48e817a004390606637ca9fb
lc-soz <lclpsoz@gmail.com>
2017-04-17 01:04:52 -0300

    Make script work again. Now using 'full run', testing all possibilities, and 'limit size' that limit the maximum size, amount of values, running only a fraction of all possibilities, but can be improve

--- Source/run.py
+++ Source/run.py
@@ -12,14 +12,14 @@ def seqTotalSize(val):
 	seqTotalSize = 0
 	for i in val:
 		if i != None:
-			seqTotalSize += seqSingle[i][0][1]
+			seqTotalSize += seqSgl[i][0][1]
 	return seqTotalSize 
 
 def seqTotalValue(val):
 	seqTotalValue = 0
 	for i in val:
 		if i != None:
-			seqTotalValue += seqSingle[i][0][0] * seqSingle[i][0][1]
+			seqTotalValue += seqSgl[i][0][0] * seqSgl[i][0][1]
 	return seqTotalValue
 
 # Amount of possible combinations with repetition per sequence size
@@ -40,45 +40,77 @@ while b <= seqMax:
 	b += 1
 print("TOTAL = " + str(seqQntSum) + '\n')
 
-# Table with all the possible sequences of a unitary value order: Values original order and size / Size and values original order
+# Table with all the possible sequences of an unitary value order by: Values order > Size AND Size > Values order
 
-seqSingleQnt = seqMax * valuesQnt
-seqSingle = []
+seqSglQnt = seqMax * valuesQnt
+seqSglVal = []
 c = 0
-while c < seqSingleQnt:
-	seqSingleSize = (c % seqMax) + 1
+while c < seqSglQnt:
+	seqSglSize = (c % seqMax) + 1
 	valuesIndex = c / seqMax
-	seqSingle.append([[values[valuesIndex], seqSingleSize], values[valuesIndex] * seqSingleSize])
-	#print(str(c) + "|" + str(valuesIndex) + "|" + str(seqSingleSize))
-	#print(str(seqSingle) + '\n')
+	seqSglVal.append([[values[valuesIndex], seqSglSize], values[valuesIndex] * seqSglSize])
+	#print(str(c) + "|" + str(valuesIndex) + "|" + str(seqSglSize))
+	#print(str(seqSglVal) + '\n')
 	c += 1
 
-seqSingleOrder = []
+seqSglSiz = []
 c = 0
-while c < seqSingleQnt:
-	valuesIndex = (c % valuesQnt)
-	seqSingleSize = (c / valuesQnt) + 1
-	seqSingleOrder.append([[values[valuesIndex], seqSingleSize], values[valuesIndex] * seqSingleSize])
-	print(str(c) + "|" + str(valuesIndex) + "|" + str(seqSingleSize))
-	print(str(seqSingleOrder) + '\n')
+while c < seqSglQnt:
+	seqSglSize = (c / valuesQnt) + 1
+	valuesIndex = c % valuesQnt
+	seqSglSiz.append([[values[valuesIndex], seqSglSize], values[valuesIndex] * seqSglSize])
+	print(str(c) + "|" + str(valuesIndex) + "|" + str(seqSglSize))
+	print(str(seqSglSiz) + '\n')
 	c += 1
 
 # Combination of single sequences to match target value
 
 seqTest = [None] * seqMax
 seqFinal = []
-seqSingleQnt = len(seqSingle)
+seqSglQnt = len(seqSglVal)
 ver = True
 stg = -1
-lmt = seqSingleQnt
-
 a = 0
 timeSec = time.time()
 while ver == True:
 	
 	seqTest[-1] = -1
 	testOverFl = False
-	while (not(all(isinstance(item, int) for item in seqTest)) or sum(seqTest) < (len(seqSingle) - 1) * seqMax):
+	while (not(all(isinstance(item, int) for item in seqTest)) or sum(seqTest) < (len(seqSglVal) - 1) * seqMax):
+		if seqTest[stg] <= seqSglQnt:
+			seqTest[stg] += 1
+		
+		if seqTest[stg] == seqSglQnt:
+			testOverFl = True
+		
+		while testOverFl:
+			seqTest[stg] = 0
+			if seqTest[stg - 1] == None:
+				seqTest[stg - 1] = 0
+			else:
+				seqTest[stg - 1] += 1
+			if seqTest[stg - 1] == seqSglQnt:
+				stg -= 1
+			else:
+				stg = -1
+				testOverFl = False		
+		a += 1
+	print("Full run: " + str(a) + ': AFT: ' + str(seqTest) + " | " + str(stg))
+	if (not(all(isinstance(item, int) for item in seqTest)) or (sum(seqTest) == (len(seqSglVal) - 1) * seqMax)):
+		ver = False
+
+seqTest = [None] * seqMax
+seqFinal = []
+lmt = len(seqSglSiz)
+ver = True
+stg = -1
+a = 0
+timeThd = time.time()
+while ver == True:
+	
+	seqTest[-1] = -1
+	testOverFl = False
+	while (not(all(isinstance(item, int) for item in seqTest)) or sum(seqTest) < ((lmt - 1) * seqMax)):
 		if seqTest[stg] < lmt:
 			seqTest[stg] += 1
 		
@@ -98,10 +130,12 @@ while ver == True:
 				stg = -1
 				testOverFl = False		
 		a += 1
-	print(str(a) + ': AFT: ' + str(seqTest) + " | " + str(stg))
-	if (not(all(isinstance(item, int) for item in seqTest)) or (sum(seqTest) == (len(seqSingle) - 1) * seqMax)):
+		print(seqTest, lmt)
+	print("Limite size: " + str(a) + ': AFT: ' + str(seqTest) + " | " + str(stg))
+	if (not(all(isinstance(item, int) for item in seqTest)) or sum(seqTest) == ((lmt - 1) * seqMax)):
 		ver = False
 
 timeLst = time.time()
 print(timeSec - timeFrt)
-print(timeLst - timeSec)
+print("Full run:   " + str(timeThd - timeSec))
+print("Limit size: " + str(timeLst - timeThd))
