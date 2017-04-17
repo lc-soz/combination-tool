46897b105b033d1d053954b1921f1a49ab8ad88b
lc-soz <lclpsoz@gmail.com>
2017-04-17 16:44:08 -0300

    Mathematical part is over, now the loop only try unique sequences, saving up orders of magnitude in time

--- Source/run.py
+++ Source/run.py
@@ -3,17 +3,17 @@ import time
 
 timeFrt = time.time()
 
-seqMax = 2
+seqMax = 13
 values = [16, 18, 19, 20, 21, 24, 25, 30]
 valuesQnt = len(values)
-targetValue = 40
+targetValue = 251
 
-def seqSglTotalValue(val):
-	seqSglTotalValue = 0
-	for i in val:
+def seqTlValue(tbl):
+	seqTlValue = 0
+	for i in tbl:
 		if i != None:
-			seqSglTotalValue += values[i]
-	return seqSglTotalValue
+			seqTlValue += values[i]
+	return seqTlValue
 
 # Amount of possible combinations (order doesn't matter) with repetition per sequence size
 
@@ -35,14 +35,14 @@ print("TOTAL = " + str(seqQntSum) + '\n')
 
 # Combination of single sequences to match target value // Combination using the given values to match targetValue
 
+#print("========FULL LOOP========")
 seqTest = [None] * seqMax
 seqFinalSgl = []
 lmt = valuesQnt
-ver = True
 stg = -1
-a = 0
+counterSgl = 0
 timeSec = time.time()
-while ver == True:
+while False:
 	
 	seqTest[-1] = -1
 	testOverFl = False
@@ -63,16 +63,71 @@ while ver == True:
 				stg -= 1
 			else:
 				stg = -1
-				testOverFl = False		
-		if seqSglTotalValue(seqTest) == targetValue:
+				testOverFl = False
+		
+		if seqTlValue(seqTest) == targetValue:
 			seqFinalSgl.append(list(seqTest))
-		print(seqTest)		
-		a += 1
-	print('\n' + "Use values: " + str(a) + ': AFT: ' + str(seqTest) + " | " + str(len(seqFinalSgl)))
-	print(seqFinalSgl)
-	if (not(all(isinstance(item, int) for item in seqTest)) or sum(seqTest) == ((lmt - 1) * seqMax)):
-		ver = False
+		#print("Sgl: ",seqTest)
+		
+		counterSgl += 1
+	break
+
+#print('\n' + "========LESS REPETITIONS========")
+
+seqTest = [None] * seqMax
+seqFinalLss = []
+lmt = valuesQnt
+stg = -1
+counterLss = 0
+timeThd = time.time()
+while True:
+	
+	seqTest[-1] = -1
+	testOverFl = False
+	while (not(all(isinstance(item, int) for item in seqTest)) or sum(seqTest) < ((lmt - 1) * seqMax)):
+		if seqTest[stg] < lmt:
+			seqTest[stg] += 1
+		
+		if seqTest[stg] == lmt:
+			testOverFl = True
+		while testOverFl:
+			seqTest[stg] = 0
+			if seqTest[stg - 1] == None:
+				seqTest[stg - 1] = 0
+				a = stg - 1
+				while a < 0:
+					seqTest[a] = seqTest[stg - 1]
+					a += 1
+				seqTest[stg] = seqTest[stg - 1]
+			else:
+				seqTest[stg - 1] += 1
+				a = stg - 1
+				while a < 0:
+					seqTest[a] = seqTest[stg - 1]
+					a += 1
+				seqTest[stg] = seqTest[stg - 1]
+			if seqTest[stg - 1] == lmt:
+				stg -= 1
+			else:
+				stg = -1
+				testOverFl = False
+		
+		if seqTlValue(seqTest) == targetValue:
+			seqFinalLss.append(list(seqTest))
+		#print("Lss: ", seqTest)
+		
+		counterLss += 1
+	break
+
+print(seqFinalSgl)
+print('')
+print(seqFinalLss)
+
+print('\n' + "Full loop: " + str(counterSgl) + ': AFT: ' + str(seqTest) + " | " + str(len(seqFinalSgl)))
+print('\n' + "Less rep.: " + str(counterLss) + ': AFT: ' + str(seqTest) + " | " + str(len(seqFinalLss)))
+
 
 timeLst = time.time()
 print('\n' + "Before runs: " + str(timeSec - timeSec))
-print("Use values:  " + str(timeLst - timeSec))
+print("Full loop:   " + str(timeThd - timeSec))
+print("Less rep.:   " + str(timeLst - timeThd))
