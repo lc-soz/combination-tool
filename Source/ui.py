e3ce66b4d6c7386d62e02674953b41f9513f94a6
lc-soz <lclpsoz@gmail.com>
2017-04-22 23:39:45 -0300

    Start to print formatted table to console

--- Source/ui.py
+++ Source/ui.py
@@ -9,22 +9,15 @@ cls()
 optChg = raw_input("Use 'all', 'some' or 'no' default values/settings? (all/some/no) ")
 
 if optChg == "no":
-	seqMax = input("Sequence size: ")
-	values = input("Values: ")
-	targetMin = input("Min target value: ")
-	targetMax = input("Max target value: ")
-	optRuns = raw_input("Type: " + '\n' + "a = Combinations, don't consider order" + '\n' + "b = Permutation, consider order of elements" + '\n' + "c = Both" + '\n' + "Opt: ")
-	optTimes = int(raw_input("Amount of runs: "))
-	optPrint = raw_input("Full text (2), Time (1), Match sequences (0): (01;21;..)")
-	optPrintSeq = raw_input("Print 'all' sequences or other amount? (all/int) ")
-	optPrintSeqSize = raw_input("Print 'all' sizes or one specific? (all/int) ")
+	optChgDef = range(1,9)
 
 if optChg == "some":
 	print "1. Sequence size\n2. Values\n3. Target Value (s)\n4. Runs options\n5. Amount of runs\n6. Print options\n7. Amount of sequences to be printed\n8. Sequences sizes to be printed"	
-	optChgSome = raw_input("Please list all values/settings you want to change: ")
+	optChgDef = raw_input("Please list all values/settings you want to change: ")
 
-if 'optChgSome' in locals():
-	for opt in optChgSome:
+if 'optChgDef' in locals():
+	for opt in optChgDef:
+		opt = str(opt)
 		if opt == '1': seqMax = input("Sequence size: ")
 		if opt == '2': values = input("Values: ")
 		if opt == '3':
@@ -42,15 +35,14 @@ for idx, val in enumerate(tuple(optPrint)):
 	optPrtVal.append(val)
 optPrint = tuple(optPrtVal)
 
-print type(optPrint)
-print optPrint
-time.sleep(3)
 cls()
-print "         OPTIONS         ", "            VALUES"
-print "Sequence size:           ", seqMax
-print "Values:                  ", values
-print "Min to Max target value: ", targetMin, " to ", targetMax
-print "Amount of runs:          ", optTimes
+print  "|", "         OPTIONS         ", "|","             VALUES              |"
+print  "|---------------------------+----------------------------------|"
+print  "|", "Sequence size:           ", "|",seqMax, "                              |"
+print  "|", "Values:                  ", "|",values, "|"
+print  "|", "Min to Max target value: ", "|",targetMin, " to ", targetMax, "                    |"
+print  "|", "Amount of runs:          ", "|",optTimes, "                               |"
+print  "|---------------------------+----------------------------------|"
 
 print "\nPRINTED:"
 print "Target Value: 'value'"
